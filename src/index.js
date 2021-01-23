32cdb65e403581c16b631c303c3fd9f7ed24fb8a
Rabets Vlad <vladrabets46@gmai.com>
2021-01-23 17:03:10 +0300

    typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
+  if(array=='' || array==[] || array==undefined)
   return 0;
+  else{
+    let min=array[0];
+    for(let i=1; i<array.length; i++)
+      if(min>array[i])
+        min=array[i];
+    return min;
+  }
 }
 
 exports.max = function max (array) {
+  if(array=='' || array==[] || array==undefined)
   return 0;
+  else{
+  let max=array[0];
+  for(let i=1; i<array.length; i++)
+    if(max<array[i])
+      max=array[i];
+  return max;
+  }
 }
 
 exports.avg = function avg (array) {
+  if(array=='' || array==[] || array==undefined)
   return 0;
+  else{
+  let sum=0;
+  for(let i=0; i<array.length; i++)
+    sum+=array[i];
+  return sum/array.length;
+  }
 }
